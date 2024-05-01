7656fdf0cdb1a1df58841d0a1f325a38aa24154f
Morteza Soleimani <msoleimani1992@gmail.com>
2024-05-01 18:20:54 +0330

    Add command-line option for specifying log file
    This commit introduces a new command-line option, '--logFile', which
    allows users to specify the path of the log file when running the
    program. This enhancement provides users with flexibility in controlling
    the location and name of the log file generated by the application

--- main.go
+++ main.go
@@ -1,27 +1,38 @@
 package main
 
 import (
-	"io/fs"
 	"log"
 	"os"
 
+	"github.com/soleimanim/gotype/logger"
 	"github.com/soleimanim/gotype/screen"
 	"github.com/soleimanim/gotype/screen/buffers"
 )
 
 func main() {
-	file, err := os.OpenFile("./log", os.O_APPEND|os.O_WRONLY, fs.ModeAppend)
-	if err != nil {
-		log.Fatal(err)
+	args := os.Args
+
+	for i, arg := range args {
+		switch arg {
+		case "--logFile":
+			if i == len(args)-1 {
+				log.Fatal("Error: The '--logFile' option cannot be used in isolation. Please provide the file path after it.")
+			}
+			path := args[i+1]
+			file, err := logger.SetLoggerFile(path)
+			if err != nil {
+				log.Fatal("Error:", err)
+			}
+			defer file.Close()
+		}
 	}
-	log.SetOutput(file)
 
 	statusBuffer := buffers.NewStatusLineBuffer()
 	actionsBuffer := buffers.NewActionsLineBuffer()
 	typingTestBuffer := buffers.NewTypingTestBuffer(buffers.TestMode25Words)
 
 	window := screen.NewWindow()
-	err = window.Init(&statusBuffer)
+	err := window.Init(&statusBuffer)
 	if err != nil {
 		panic(err)
 	}
@@ -39,4 +50,8 @@ func main() {
 
 		window.Draw()
 	}
+}
+
+func handleCommandLineArgs() {
+
 }
